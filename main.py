d9cf3e3cf48b402605114394e385621f336660dd
Valera <valerylarionov06@gmail.com>
2024-04-22 03:17:26 +0300

    web_version 47.0

--- main.py
+++ main.py
@@ -1,48 +1,51 @@
 from flask import Flask, render_template
 import random
+import os
 
 app = Flask(__name__)
 
 
 def get_text():
-    words = open('words_text.txt', encoding='utf8').readline()
-    words = words[1:-1]
-    words = words.split(", ")
-    words = list(map(lambda x: x[1:-1], words))  # адекватное преобразование в список
-    words = words[0:-1]
-    slov = {}
-    words = list(map(lambda x: x.lower(), words))  # к строчным
-    for i in range(len(words)):  # в словарь
-        if words[i] not in slov and words[i] != words[-1]:
-            slov[words[i]] = []
-        if words[i] != words[-1]:
-            slov[words[i]].append(words[i + 1])
-    text = []
-    keys = list(slov)  # список ключей
-    maybe = random.choice(keys)  # первое слово
-    text.append(maybe)
-    n = random.randint(150, 300)
-    for i in range(n + 30):  # для того чтобы учесть запятые и прочее ненужные элементы
-        maybe = text[-1]  # обычное слово
-        if maybe not in slov:
-            while True:  # если нет ключа
-                maybe = random.choice(slov[random.choice(keys)])
-                if maybe in slov:
-                    break
-        word = random.choice(slov[maybe])
-        text.append(word)  # список слов
-    for i in range(len(text)):
-        if text[i] in ",.!?:;":  # склейка знаков препенания
-            text[i - 1] = text[i - 1] + text[i]
-    correst_words = list(filter(lambda x: x[0].isalnum(), text))  # чистка от знаков
-    correst_words[0] = correst_words[0].capitalize()  # предложение с большой буквы
-    for i in range(len(correst_words) - 1):
-        if correst_words[i][-1] == ".":
-            correst_words[i + 1] = correst_words[i + 1].capitalize()
-    correst_words = correst_words[0:n]
-    if correst_words[-1][-1] in ",.!?:;":  # если в конце знак препенания, то удалить
-        correst_words[-1] = correst_words[-1][0:-1]
-    return " ".join(correst_words) + "..."
+    file_path = os.path.join(os.getcwd(), 'words_text.txt')
+    with open(file_path, 'r', encoding='utf8') as file:
+        words = file.readline()
+        words = words[1:-1]
+        words = words.split(", ")
+        words = list(map(lambda x: x[1:-1], words))  # адекватное преобразование в список
+        words = words[0:-1]
+        slov = {}
+        words = list(map(lambda x: x.lower(), words))  # к строчным
+        for i in range(len(words)):  # в словарь
+            if words[i] not in slov and words[i] != words[-1]:
+                slov[words[i]] = []
+            if words[i] != words[-1]:
+                slov[words[i]].append(words[i + 1])
+        text = []
+        keys = list(slov)  # список ключей
+        maybe = random.choice(keys)  # первое слово
+        text.append(maybe)
+        n = random.randint(150, 300)
+        for i in range(n + 30):  # для того чтобы учесть запятые и прочее ненужные элементы
+            maybe = text[-1]  # обычное слово
+            if maybe not in slov:
+                while True:  # если нет ключа
+                    maybe = random.choice(slov[random.choice(keys)])
+                    if maybe in slov:
+                        break
+            word = random.choice(slov[maybe])
+            text.append(word)  # список слов
+        for i in range(len(text)):
+            if text[i] in ",.!?:;":  # склейка знаков препенания
+                text[i - 1] = text[i - 1] + text[i]
+        correst_words = list(filter(lambda x: x[0].isalnum(), text))  # чистка от знаков
+        correst_words[0] = correst_words[0].capitalize()  # предложение с большой буквы
+        for i in range(len(correst_words) - 1):
+            if correst_words[i][-1] == ".":
+                correst_words[i + 1] = correst_words[i + 1].capitalize()
+        correst_words = correst_words[0:n]
+        if correst_words[-1][-1] in ",.!?:;":  # если в конце знак препенания, то удалить
+            correst_words[-1] = correst_words[-1][0:-1]
+        return " ".join(correst_words) + "..."
 
 
 @app.route('/', methods=['GET', 'POST'])
