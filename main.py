9b7dc0640c7e0749ba24dd60f1af3ac39d4c8a84
Valera <valerylarionov06@gmail.com>
2024-05-05 00:01:31 +0300

    pythonanywhere

--- main.py
+++ main.py
@@ -26,7 +26,7 @@ def get_text():
         keys = list(slov)  # список ключей
         maybe = random.choice(keys)  # первое слово
         text.append(maybe)
-        n = random.randint(150, 300)
+        n = random.randint(250, 350)
         for i in range(n + 30):  # для того чтобы учесть запятые и прочее ненужные элементы
             maybe = text[-1]  # обычное слово
             if maybe not in slov:
